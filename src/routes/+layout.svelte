e6082eea01e08ef466e2457971667d18cc935afa
joaquimnetocel <joaquim.neto.cel@gmail.com>
2023-08-09 16:21:15 -0300

    VERSION 0.0.10

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -51,7 +51,7 @@
 	<svelte-fragment slot="slotFooter">
 		<div class="flex items-center justify-between h-8">
 			<div>All Rights Reserved ©</div>
-			<div>VERSION 0.0.4</div>
+			<div>VERSION 0.0.10</div>
 		</div>
 	</svelte-fragment>
 </SvelteBird>
