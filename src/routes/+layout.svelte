44cdd02c1ebb19f40a7361bf0aa1cebf30d2f90a
joaquimnetocel <joaquim.neto.cel@gmail.com>
2024-09-15 11:31:44 -0300

    TAILWIND

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../app.css';
+	// TAILWIND CONFIG.
+
+	let {
+		children,
+	}: {
+		children: Snippet;
+	} = $props();
+</script>
+
+{@render children()}
